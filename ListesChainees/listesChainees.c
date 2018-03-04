5c221d1b66017db61f9283559b8124cd02c7cfd1
Natalia Tatarinova <sacora@gmail.com>
2018-03-04 23:16:36 +0100

    ...

--- ListesChainees/listesChainees.c
+++ ListesChainees/listesChainees.c
@@ -1,7 +1,7 @@
 /*
  ============================================================================
  Name        : listesChainees.c
- Author      : sam
+ Author      : natasha
  Version     :
  Copyright   : Your copyright notice
  Description : Hello World in C, Ansi-style
